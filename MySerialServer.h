1225140c5570091255447bd15a6c19c7f95335cf
EliadSellem <58472892+Eliadsellem@users.noreply.github.com>
2020-01-07 10:10:39 +0200

    Update MySerialServer.h

--- MySerialServer.h
+++ MySerialServer.h
@@ -13,10 +13,10 @@ using namespace std;
 using namespace server_side;
 class MySerialServer: public Server {
  public:
-  virtual void open(int p, ClientHandler c);
+  virtual void open(int p, ClientHandler* c);
   virtual void stop();
-  void start();
-  void writeIntoFile(ofstream& out, char * buffer ,int size);
+//  void writeIntoFile(string fileName, char * buffer ,int size);
+  char* readFromFile();
 };
 
 #endif //MILE_STONE2__MYSERIALSERVER_H_
